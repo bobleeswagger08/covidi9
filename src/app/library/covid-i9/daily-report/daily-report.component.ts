a784898aced07b36c7075d01b7c650575bae05f2
NishanAsim <56675100+NishanAsim@users.noreply.github.com>
2020-03-30 20:51:56 +0530

    Export to Excel feature in daily report

--- src/app/library/covid-i9/daily-report/daily-report.component.ts
+++ src/app/library/covid-i9/daily-report/daily-report.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef, ElementRef } from '@angular/core';
 import { IListCandidate, IListUPHC, IListWard, CandidateReportFilter, CandidateDateWiseReport } from '../model/candidate-input';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { CovidI9Service } from '../services/covid-i9.service';
+import * as XLSX from "xlsx";
 
 @Component({
   selector: 'app-daily-report',
@@ -13,7 +14,15 @@ import { CovidI9Service } from '../services/covid-i9.service';
 export class DailyReportComponent implements OnInit {
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
-  displayedColumns: string[] = ['source', 'serialNo', 'dateOfArival', 'name', 'mobileNo', 'uphc','isEverContacted', 'dateOfContacted'];
+  @ViewChild('summaryTable', { static: false }) reportTable: ElementRef;
+  // displayedColumns: string[] = ['source', 'serialNo', 'dateOfArival', 'name', 'mobileNo', 'uphc','isEverContacted', 'dateOfContacted'];
+  displayedColumns: string[] = ['source', 'serialNo','name', 'flightNumber', 'countryVisited','dob','sex','flightNo','arivalDate',
+  'mobileNo','address','finalDestination','block','state','isEverContacted','isContactedOnCurrentDate',
+  'isSymptomatic','isReferredForMedicalCare','reasonForUnableToTrace','isReleasedFromSurveillanc',
+  'wardNo','uphc','commentByMOIC'];
+
+  selectedDate : Date= new Date();
+
   listCandidate: CandidateDateWiseReport[];
   listOfUpHC: IListUPHC[] = [];
   listOfWards: IListWard[] = [];
@@ -22,8 +31,7 @@ export class DailyReportComponent implements OnInit {
   dataSource: MatTableDataSource<CandidateDateWiseReport>;
   constructor(private router: Router, private cdr: ChangeDetectorRef
     , private SpinnerService: NgxSpinnerService, private covidService: CovidI9Service) {
-      this.reportFilter.reportStartDate=new Date();
-      this.reportFilter.reportEndDate=new Date('2020-03-10T00:00:00');
+     
   }
 
   ngOnInit() {
@@ -31,6 +39,9 @@ export class DailyReportComponent implements OnInit {
   }
 
   getReportData() {
+    this.reportFilter.reportStartDate=this.selectedDate;
+    this.reportFilter.reportEndDate=this.selectedDate;
+    
     this.covidService.getDailyReportData(this.reportFilter)
       .subscribe((cList: CandidateDateWiseReport[]) => {
         //this.listServiceRequest = srList;
@@ -52,6 +63,36 @@ export class DailyReportComponent implements OnInit {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  }
+
+  exportToExcel() {
+
+    //const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.reportTable.nativeElement);
+    // const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.reportTable.nativeElement);
+    // const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    // XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    // /* save to file */
+    // XLSX.writeFile(wb, 'Daily Summary.xlsx');
+   this.exportToExcelWithId("reportTable","Daily Status Summary");
+
+  }
+  // downloadaspdf() {
+
+  //   // const documentDefinition = { content: 'This is an sample PDF printed with pdfMake' };
+  //   const documentDefinition = this.getDocumentDefinition();
+  //   pdfMake.createPdf(documentDefinition).open();
+  //   // pdfMake.createPdf(documentDefinition).download();
+  //   // pdfMake.createPdf(documentDefinition).print();
+  // }
+
+   exportToExcelWithId(tableId: string, name?: string) {
+    let timeSpan = new Date().toISOString();
+    let prefix = name || "ExportResult";
+    let fileName = `${prefix}-${timeSpan}`;
+    let targetTableElm = document.getElementById(tableId);
+    let wb = XLSX.utils.table_to_book(targetTableElm, <XLSX.Table2SheetOpts>{ sheet: prefix });
+    XLSX.writeFile(wb, `${fileName}.xlsx`);
   }
 
 }
